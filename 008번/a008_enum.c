2a8b44ddec9adc99a360ef465297b1961d2f5166
nemo <nemo724@naver.com>
2025-01-16 22:11:44 +0900

    카푸치노의 사이즈별 가격을 담은 int배열 Price_of_Cappucino 추가

--- 008번/a008_enum.c
+++ 008번/a008_enum.c
@@ -2,3 +2,4 @@
 enum Size{Short,Tall, Grande, Venti};
 const char SizeName[][10] = {"Short","Tall","Grande","venti"};
 int Pirce_of_Americano[4] = {3800, 4100, 4600, 5100};
+int Price_of_Cappucino[4] = { 4600, 5900,6400, 6900 };
